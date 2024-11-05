5e07ade6322a2616a7b9130d6a94416635a1f647
allenTong31415926 <allen.tong31415926@gmail.com>
2024-11-05 21:49:23 +1100

    Remove debug loggers

--- tournament/signals.py
+++ tournament/signals.py
@@ -1,7 +1,6 @@
 # tournament/signals.py
 
 import os
-import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Tournament
@@ -14,14 +13,10 @@ es = Elasticsearch(os.getenv("ELASTICSEARCH_URL", "http://localhost:9200"))
 @receiver(post_save, sender=Tournament)
 def index_tournament_in_elasticsearch(sender, instance, **kwargs):
     # Prepare the document for Elasticsearch
-    logger = logging.getLogger()
-    logger.debug("====================================")
     doc = {
         "sports": instance.get_sports_display().replace(" ", "_").lower(),
         "format": instance.get_format_display().replace(" ", "_").lower(),
         "team_player": instance.get_team_player_display().replace(" ", "_").lower(),
     }
     # Index the document in Elasticsearch
-    logger.debug("====================================1111")
     es.index(index="tournaments", id=instance.id, document=doc)
-    logger.debug("====================================22222")
